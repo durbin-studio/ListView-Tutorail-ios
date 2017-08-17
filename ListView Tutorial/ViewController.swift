22aa0f6509c9a2fbc64b678c6f7bfff7a0880784
Waheduzzaman <waheduzzaman@Admins-MacBook-Pro-2.local>
2017-08-18 00:24:36 +0600

    added tableview and related controllers:

--- ListView Tutorial/ViewController.swift
+++ ListView Tutorial/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var itemArray = ["Riashad","Wahed", "Rafique", "Shafique","zaman","noor","mac","pro","linux"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //do some networking stuff or load data from database and create a list or array
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //mendatory because table view needs to know how many rows it will be working on
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count;
+    }
 
+
+    //mendatory because table view needs to know what are those cell item and what the contains
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier:"defaultCell"); //using the default cell provided by ios
+        cell.textLabel?.text = itemArray[indexPath.row];
+        return cell;
+        
+    }
 }
 
